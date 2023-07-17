c34d745c393196c9eca61bad5df2b37beb5f5767
IvanCastillo1986 <total180training@gmail.com>
2023-07-17 05:54:41 -0400

    added Show route, improved Index route

--- controllers/snesGamesController.js
+++ controllers/snesGamesController.js
@@ -5,7 +5,29 @@ const snesGamesArray = require("../models/snesGames");
 
 // Index
 snesGames.get("/", (req, res) => {
-    res.send(snesGamesArray);
+    try {
+        res.status(200).json({ data: snesGamesArray });
+    } catch(err) {
+        res.status(500).json({ error: err.message });
+    }
 });
+
+// Show  /snesGames/1
+snesGames.get("/:idx", (req, res) => {
+    try {
+        const {idx} = req.params;
+        const game = snesGamesArray[idx];
+
+        if (game) {
+            res.status(200).json({ data: game });
+        } else {
+            res.status(404).json({ error: `could not find game at index ${idx}` });
+        }
+    } catch(err) {
+        res.status(500).json({ error: message.err })
+    }
+});
+
+
 
 module.exports = snesGames;
