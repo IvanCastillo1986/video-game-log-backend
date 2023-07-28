23ecda22a5a864c295aaa5a12a4fe17dddff68b3
IvanCastillo1986 <total180training@gmail.com>
2023-07-28 00:12:33 -0400

    snesGamesController - added create, delete, update routes

--- controllers/snesGamesController.js
+++ controllers/snesGamesController.js
@@ -6,7 +6,7 @@ const snesGamesArray = require("../models/snesGames");
 // Index
 snesGames.get("/", (req, res) => {
     try {
-        res.status(200).json({ data: snesGamesArray });
+        res.status(200).json({ snesGamesArray });
     } catch(err) {
         res.status(500).json({ error: err.message });
     }
@@ -19,7 +19,7 @@ snesGames.get("/:idx", (req, res) => {
         const game = snesGamesArray[idx];
 
         if (game) {
-            res.status(200).json({ data: game });
+            res.status(200).json({ game });
         } else {
             res.status(404).json({ error: `could not find game at index ${idx}` });
         }
@@ -27,6 +27,40 @@ snesGames.get("/:idx", (req, res) => {
         res.status(500).json({ error: message.err })
     }
 });
+
+// Create
+snesGames.post("/", (req, res) => {
+    const game = req.body;
+    snesGamesArray.push(game);
+
+    res.json(snesGamesArray[snesGamesArray.length - 1]);
+});
+
+// Delete
+snesGames.delete("/:idx", (req, res) => {
+    const {idx} = req.params;
+
+    if (snesGamesArray[idx]) {
+        console.log('delete index: ', idx);
+        const deletedBookmark = snesGamesArray.splice(idx, 1);
+        res.status(200).json(deletedBookmark);
+    } else {
+        res.status(404).json({error: 'index not found'});
+    }
+});
+
+// Update
+snesGames.put("/:idx", (req, res) => {
+    const {idx} = req.params;
+
+    if (snesGamesArray[idx]) {
+        const newGame = snesGamesArray[idx] = req.body
+        res.status(200).json(newGame)
+    } else {
+        res.status(404).json({error: 'index not found'});
+    }
+});
+
 
 
 
--- models/snesGames.js
+++ models/snesGames.js
@@ -1,18 +1,18 @@
 module.exports = [
     {
-        name: "Super C",
+        title: "Super C",
         region: "NTSC",
         date_released: "April 1990",
         cover_art: "url to be found later"
     },
     {
-        name: "Super Mario RPG",
+        title: "Super Mario RPG",
         region: "PAL",
         date_released: "May 1996",
         cover_art: "url to be found later"
     },
     {
-        name: "Zelda: A Link to the Past",
+        title: "Zelda: A Link to the Past",
         region: "NTSC",
         date_released: "April 1992",
         cover_art: "url to be found later"
